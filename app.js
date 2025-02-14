71a898457fb792b4a3ce079c9bf734f4e2da484d
Kaya Woods <kayanevada@gmail.com>
2025-02-13 16:58:15 -0800

    basics down

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const mongoose = require('mongoose');
-const Customer = require('./models/customer')
+const Customer = require('./customer');
+// const prompt = require('prompt-sync')()
 
-const customerSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-  });
+// const welcome = prompt('Welcome to the CRM\n1. Create a customer\n2. View all customers\n3. Update a customer\n4. Delete a customer\n5. quit')
 
-  const connect = async () => {
-    // Connect to MongoDB using the MONGODB_URI specified in our .env file.
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('Connected to MongoDB');
+const connect = async () => {
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log('Connected to MongoDB');
+
+  await runQueries();
+
+  await mongoose.disconnect();
+  console.log('Disconnected from MongoDB');
+
+  process.exit()
+
+
+}
+const runQueries = async () => {
+  console.log('Queries running.')
+  await findCustomers(); 
+  await createCustomer()
+};
+
+connect()
+
+const createCustomer = async () => {
+  const customerData = {
+      name: 'Kaya', age: 100, 
+      name:'Santa', age: 101,
+  }
+
+  const customer = await Customer.create(customerData);   
+  console.log('New customer;', customer); 
+}
+
+const findCustomers = async () => {
+const customers = await Customer.find({}); 
+console.log('All customers:', customers); 
+}
